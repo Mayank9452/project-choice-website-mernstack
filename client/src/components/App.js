73509a27a565f4c3232a7eb495cce83ca0ab3c61
ashu <ashutosh.kuma_cs18>
2021-04-16 23:53:06 +0530

    create userdashboard filie user dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,21 +5,40 @@ import Header from './Header';
 import Home from './Home';
 import Signup from './Signup';
 import Signin from './Signin';
+import UserDashboard from './UserDashboard';
+import AdminDashboard from './AdminDashboard';
 import NotFound from './NotFound';
 
 
-const App = () => ( 
-  <BrowserRouter>
-    <Header />
-    <main>
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/signin' component={Signin} />
-            <Route component={NotFound} />
-        </Switch>
-    </main>
-  </BrowserRouter>
+const App = () => ( <
+    BrowserRouter >
+    <
+    Header / >
+    <
+    main >
+    <
+    Switch >
+    <
+    Route exact path = '/'
+    component = { Home }
+    /> <
+    Route exact path = '/signup'
+    component = { Signup }
+    /> <
+    Route exact path = '/signin'
+    component = { Signin }
+    /> <
+    Route exact path = '/user/dashboard'
+    component = { UserDashboard }
+    /> <
+    Route exact path = '/admin/dashboard'
+    component = { AdminDashboard }
+    /> <
+    Route component = { NotFound }
+    /> <
+    /Switch> <
+    /main> <
+    /BrowserRouter>
 );
 
 export default App;
