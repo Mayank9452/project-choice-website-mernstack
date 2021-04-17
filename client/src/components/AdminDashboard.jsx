519adeb5f5056643fb895fab91e2da7d4728b4bb
ashu <ashutosh.kuma_cs18>
2021-04-18 00:45:37 +0530

    add button for button action

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
 
 const AdminDashboard = () => {
+    // ###### view #####
+    const showHeader = () => (
+        
+        <div className='bg-dark text-white py-4'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <h1>
+                            <i className='fas fa-home'> Dashboard</i>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+    const  showActionBtns = () => (
+        <div className='bg-light my-2'>
+            <div className='container'>
+                <div className='row pb-3'>
+                    <div className='d-grid gap-2 col-md-4 my-1'>
+                        <button className='btn btn-outline-info btn-block'>
+                            <i className='fas fa-plus'> Add Category</i>
+                        </button>
+                    </div>
+
+                    <div className='d-grid gap-2 col-md-4 my-1'>
+                        <button className='btn btn-outline-warning btn-block'>
+                            <i className='fas fa-plus'> Add second cat</i>
+                        </button>
+                    </div>
+
+                    <div className='d-grid gap-2 col-md-4 my-1'>
+                        <button className='btn btn-outline-success btn-block'>
+                            <i className='fas fa-money-check-alt'>  View Order</i>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+//// ##### RENDER ######
     return (
-        <div>Inside AdminDashboard</div>
+        <section>
+            { showHeader() }
+            { showActionBtns() }
+        </section>
     )
-}
+};
 
 export default AdminDashboard;
