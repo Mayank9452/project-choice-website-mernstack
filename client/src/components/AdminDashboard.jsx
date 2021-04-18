48c7f31894c03fac2b72902d4206eeea10fb3e9b
ashu <ashutosh.kuma_cs18>
2021-04-18 22:42:08 +0530

    showProjectModal function is completed and some change sin header.jsx

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -77,8 +77,8 @@ const AdminDashboard = () => {
                     </div>
 
                     <div className='d-grid gap-2 col-md-4 my-1'>
-                        <button type="button" className='btn btn-outline-warning'>
-                            <i className='fas fa-plus'> Add second cat</i>
+                        <button type="button" className='btn btn-outline-warning' data-bs-toggle='modal' data-bs-target='#addProjectModal'>
+                            <i className='fas fa-plus'> Add Project</i>
                         </button>
                     </div>
 
@@ -131,12 +131,81 @@ const AdminDashboard = () => {
         </div>
     );
 
+
+
+    const showProjectModal = () => (
+        <div id='addProjectModal' className='modal' onClick={handleMessages}>
+            <div className='modal-dialog modal-dialog-centered modal-lg'>
+                <div className='modal-content'>
+                    <form onSubmit={handleCategorySubmit}>
+                        <div className='modal-header bg-warning text-white'>
+                            <h5 className='modal-title'>Add Category</h5>
+                            <button className='close' data-bs-dismiss='modal'>
+                                <span><i className='fas fa-times'></i></span>
+                            </button>
+                        </div>
+                        <div className='modal-body'>
+                            {errorMsg && showErrorMsg(errorMsg)}
+                            {successMsg && showSuccessMsg(successMsg)}
+                            {
+                                loading ?  (
+                                    <div className='text-center'>{showLoading()}</div>
+                                ) : (
+                                    <Fragment>
+                                        <div className="input-group mb-3">
+                                            <label className="input-group-text" for="inputGroupFile01">Upload</label>
+                                            <input type="file" className="form-control" id="inputGroupFile01" />
+                                        </div>
+                                        <div className='form-group mb-3'>
+                                            <label className='text-secondary'>Name</label>
+                                            <input type="text" className='form-control' />
+                                        </div>
+                                        <div className='form-group mb-3'>
+                                            <label className='text-secondary'>Description</label>
+                                            <textarea name="" id="" cols="30" rows="3" className='form-control'></textarea>
+                                        </div>
+                                        <div className='form-group mb-3'>
+                                            <label className='text-secondary'>Price</label>
+                                            <input type="text" className='form-control' />
+                                        </div>
+                                        <div className='row'>
+                                            <div className='form-group col mb-2'>
+                                                <label className='text-secondary mb-2'>Category</label>
+                                                <select className='form-select'>
+                                                    <option>Choose one...</option>
+                                                    <option>fisrt</option>
+                                                    <option>second</option>
+                                                    <option>third</option>
+                                                </select>
+                                            </div>
+                                            <div className='form-group col'>
+                                                <label className='text-secondary mb-2'>Quantity</label>
+                                                <input type='number' className='form-control' min='0' max='1000'/>
+                                            </div>
+                                        </div>
+                                    </Fragment>
+                                )
+                            }
+                            
+                            
+                        </div>
+                        <div className='modal-footer'>
+                            <button className='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                            <button type='submit' className='btn btn-warning text-white'>Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
 //// ##### RENDER ######
     return (
         <section>
             { showHeader() }
             { showActionBtns() }
             { showCategoryModal() }
+            { showProjectModal() }
         </section>
     )
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = ({ history }) => {
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     {!isAuthenticated() && (
                         <Fragment>
                             <li className="nav-item">
