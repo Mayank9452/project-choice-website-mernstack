98022b557c62cfab77ad5edf8e88d15fb1483b57
ashu <ashutosh.kuma_cs18>
2021-04-18 12:16:01 +0530

    route for category.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./database/db');
 const authRoutes = require('./routes/auth');
+const categoryRoutes = require('./routes/category');
 
 
 //middleware
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/category', categoryRoutes);
 
 connectDB();
 
