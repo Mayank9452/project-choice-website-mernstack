c9dc8050bcf4bc2f92e56f7470e40eb3cdea3cbc
ashu <ashutosh.kuma_cs18>
2021-04-18 11:02:48 +0530

    create buttons

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -22,19 +22,19 @@ const AdminDashboard = () => {
             <div className='container'>
                 <div className='row pb-3'>
                     <div className='d-grid gap-2 col-md-4 my-1'>
-                        <button className='btn btn-outline-info btn-block'>
+                        <button type="button" className='btn btn-outline-info' data-bs-toggle='modal' data-bs-target='#addCategoryModal'>
                             <i className='fas fa-plus'> Add Category</i>
                         </button>
                     </div>
 
                     <div className='d-grid gap-2 col-md-4 my-1'>
-                        <button className='btn btn-outline-warning btn-block'>
+                        <button type="button" className='btn btn-outline-warning'>
                             <i className='fas fa-plus'> Add second cat</i>
                         </button>
                     </div>
 
                     <div className='d-grid gap-2 col-md-4 my-1'>
-                        <button className='btn btn-outline-success btn-block'>
+                        <button type="button" className='btn btn-outline-success'>
                             <i className='fas fa-money-check-alt'>  View Order</i>
                         </button>
                     </div>
@@ -43,11 +43,39 @@ const AdminDashboard = () => {
             </div>
         </div>
     );
+
+
+    const showCategoryModal = () => (
+        <div id='addCategoryModal' className='modal'>
+            <div className='modal-dialog modal-dialog-centered modal-lg'>
+                <div className='modal-content'>
+                    <div className='modal-header bg-info text-white'>
+                        <h5 className='modal-title'>Add Category</h5>
+                        <button className='close' data-bs-dismiss='modal'>
+                            <span><i className='fas fa-times'></i></span>
+                        </button>
+                    </div>
+                    <div className='modal-body'>
+                        <form>
+                            <label className='text-secondary'>Category</label>
+                            <input type='text' className='form-control' />
+                        </form>
+                    </div>
+                    <div className='modal-footer'>
+                        <button className='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                        <button className='btn btn-info'>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
 //// ##### RENDER ######
     return (
         <section>
             { showHeader() }
             { showActionBtns() }
+            { showCategoryModal() }
         </section>
     )
 };
