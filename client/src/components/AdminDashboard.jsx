f0963435d9aaa6f9f988ef5ddc680c473208de2c
ashu <ashutosh.kuma_cs18>
2021-04-18 13:01:34 +0530

    handling the errormsg and success message while enterning the category

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { createCategory } from '../api/category';
+import isEmpty from 'validator/lib/isEmpty';
+import {showErrorMsg, showSuccessMsg } from '../helper/message';
+import { showLoading } from '../helper/loading';
+// import { response } from 'express';
 
 const AdminDashboard = () => {
 
     const [category, setCategory] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
+    const [loading, setLoading] = useState(false);
 
     //****event handler */
+
+    const handleMessages = evt => {
+        setErrorMsg('');
+        setSuccessMsg('');
+    }
+
     const handleCategoryChange = evt => {
+        setErrorMsg('');
+        setSuccessMsg('');
         setCategory(evt.target.value);
         // console.log(category);
     }
@@ -14,9 +29,24 @@ const AdminDashboard = () => {
     const handleCategorySubmit = evt => {
         evt.preventDefault();
         // console.log(category);
-        const data ={ category }
-        createCategory(data);
-    }
+        // setLoading(true);
+        if (isEmpty(category)) {
+            setErrorMsg('Please enter a category')
+        } else {
+            const data ={ category }
+            setLoading(true);
+            createCategory(data)
+                .then(response => {
+                    setLoading(false);
+                    setSuccessMsg(response.data.successMessage)
+                })
+                .catch(err => {
+                    setLoading(false);
+                    setErrorMsg(err.response.data.errorMessage);
+                })
+        }
+
+    };
 
 
     // ###### view #####
@@ -64,26 +94,36 @@ const AdminDashboard = () => {
 
 
     const showCategoryModal = () => (
-        <div id='addCategoryModal' className='modal'>
+        <div id='addCategoryModal' className='modal' onClick={handleMessages}>
             <div className='modal-dialog modal-dialog-centered modal-lg'>
                 <div className='modal-content'>
-                <form onSubmit={handleCategorySubmit}>
-                    <div className='modal-header bg-info text-white'>
-                        <h5 className='modal-title'>Add Category</h5>
-                        <button className='close' data-bs-dismiss='modal'>
-                            <span><i className='fas fa-times'></i></span>
-                        </button>
-                    </div>
-                    <div className='modal-body'>
-                        
-                            <label className='text-secondary'>Category</label>
-                            <input type='text' className='form-control' name='category' value={category} onChange={handleCategoryChange} />
-                        
-                    </div>
-                    <div className='modal-footer'>
-                        <button className='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
-                        <button type='submit' className='btn btn-info'>Submit</button>
-                    </div>
+                    <form onSubmit={handleCategorySubmit}>
+                        <div className='modal-header bg-info text-white'>
+                            <h5 className='modal-title'>Add Category</h5>
+                            <button className='close' data-bs-dismiss='modal'>
+                                <span><i className='fas fa-times'></i></span>
+                            </button>
+                        </div>
+                        <div className='modal-body'>
+                            {errorMsg && showErrorMsg(errorMsg)}
+                            {successMsg && showSuccessMsg(successMsg)}
+                            {
+                                loading ?  (
+                                    <div className='text-center'>{showLoading()}</div>
+                                ) : (
+                                    <Fragment>
+                                        <label className='text-secondary'>Category</label>
+                                        <input type='text' className='form-control' name='category' value={category} onChange={handleCategoryChange} />
+                                    </Fragment>
+                                )
+                            }
+                            
+                            
+                        </div>
+                        <div className='modal-footer'>
+                            <button className='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                            <button type='submit' className='btn btn-info'>Submit</button>
+                        </div>
                     </form>
                 </div>
             </div>
