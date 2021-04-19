bbf06f225e38eb0caaf6ff000ce1ea9ac6472cef
ashu <ashutosh.kuma_cs18>
2021-04-19 17:05:10 +0530

    destructre incomin data, create new instance for product, save data

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,38 @@
+const Product = require('../models/Product');
+
+
 exports.create = async(req, res) => {
     console.log('req.body: ', req.body);
     console.log('req.file: ', req.file);
     console.log('req.user: ', req.user);
 
+    const { filename } = req.file;
+    const { productName, productDesc, productPrice, productCategory, productQty } = req.body;
 
-    res.json({
-        message: 'Inside productController',
-    });
+    try {
+        let product = new Product();
+        product.fileName = filename;
+        product.productName = productName;
+        product.productDesc = productDesc;
+        product.productPrice = productPrice;
+        product.productCategory = productCategory;
+        product.productQty = productQty;
+
+        await product.save();
+
+        res.json({
+            successMessage: `${productName} was created`,
+            product
+        })
+    } catch (err) {
+        console.log(err, 'productController.create error');
+        res.statud(500).json({
+            errorMessage: 'Please try again later'
+        })
+    }
+
+
+    // res.json({
+    //     message: 'Inside productController',
+    // });
 };
