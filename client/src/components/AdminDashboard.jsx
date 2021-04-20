5e5cfae8859690b94e5c15f52b672c81028af52a
ashu <ashutosh.kuma_cs18>
2021-04-20 13:56:20 +0530

    add getCategories and getProducts in admin dashboard

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // calling component
 import AdminHeader from './AdminHeader';
 import AdminActionBtns from './AdminActionBtns';
 import AdminCategoryModal from './AdminCategoryModal';
 import AdminProjectModal from './AdminProjectModal';
+//redux
+import { useDispatch } from "react-redux";
+import { getCategories } from "../redux/actions/categoryActions";
+import { getProducts } from "../redux/actions/productActions";
 
 
-const AdminDashboard = () => (
+const AdminDashboard = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch]);
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch]);
+
+    return(
         <section>   
             <AdminHeader />
             <AdminActionBtns />
             <AdminCategoryModal />
             < AdminProjectModal />
         </section>
-);
+    );
+};
 
 export default AdminDashboard;
