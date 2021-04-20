944439dc2006dc80f019bec8652926a1b1f0522f
ashu <ashutosh.kuma_cs18>
2021-04-20 14:35:20 +0530

    design userIneterface for products

--- client/src/components/UserDashboard.jsx
+++ client/src/components/UserDashboard.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AdminBody from './AdminBody';
+//redux
+import { useDispatch } from "react-redux";
+import { getCategories } from "../redux/actions/categoryActions";
+import { getProducts } from "../redux/actions/productActions";
 
 const UserDashboard = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch]);
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch]);
     return (
-        <div>Inside UserDashboard</div>
+        <section>   
+            {/* <AdminHeader />
+            <AdminActionBtns />
+            <AdminCategoryModal />
+            <AdminProjectModal /> */}
+            <AdminBody />
+        </section>
     )
 }
 
