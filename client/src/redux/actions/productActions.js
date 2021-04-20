71947175b39e791ebf45a1780c921304ae67f760
ashu <ashutosh.kuma_cs18>
2021-04-20 13:39:11 +0530

    create action for getProducts

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -4,6 +4,7 @@ import {
     SHOW_ERROR_MESSAGE,
     SHOW_SUCCESS_MESSAGE,
 } from '../constants/messageConstants';
+import { GET_PRODUCTS } from "../constants/productConstants";
 
 export const createProduct = formData => async dispatch => {
     try {
@@ -17,4 +18,19 @@ export const createProduct = formData => async dispatch => {
         dispatch({ type: STOP_LOADING });
         dispatch({ type: SHOW_ERROR_MESSAGE, payload: err.response.data.errorMessage });
     }
-}
+};
+
+
+export const getProducts = () => async dispatch => {
+    try {
+        dispatch({ type: START_LOADING })
+        const response = await axios.get('/api/product');
+        dispatch({ type: STOP_LOADING })
+        dispatch({ type: GET_PRODUCTS, payload: response.data.products })
+
+    } catch (err) {
+        console.log('getProducts api error: ', err);
+        dispatch({ type: STOP_LOADING });
+        dispatch({ type: SHOW_ERROR_MESSAGE, payload: err.response.data.errorMessage });
+    }
+};
