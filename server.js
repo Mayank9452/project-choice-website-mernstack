c71042abbf31d277770ca56cf2c6c298917327fb
ashu <ashutosh.kuma_cs18>
2021-04-20 15:10:13 +0530

    add static to server.js to get access images from / uploads

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
 
 connectDB();
 
