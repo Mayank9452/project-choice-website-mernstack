ce3077dc5dd5b1936c39718725c5edee73c3f4da
Mayank Kumar Singh <mayank.singh_cs18@gla.ac.in>
2021-04-22 01:41:29 +0530

    importing deleteProduct function from redux/actions/production

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // redux
 import { useDispatch } from 'react-redux';
+import { deleteProduct } from '../redux/actions/productActions';
